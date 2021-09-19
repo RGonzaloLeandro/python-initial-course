01070a9f8e0d273e8c4510321b741cf683d79f5a
RGonzaloLeandro <rgonzaloleandro@gmail.com>
2021-09-19 11:16:58 -0300

    check-consulta-funcion

--- Check.py
+++ Check.py
@@ -1,6 +1,6 @@
 def suma(a, b):
     s = a + b
-    return s #para que sirve linea en este caso del ejemplo?... el resultado de la funcion se almacena en la variable s?... no se usa nunca esa variable
+    return s #para que sirve linea en este caso del ejemplo?... El resultado de la funcion se almacena en la variable s pero no se usa nunca esa variable... cuando el control vuelve al flujo principal del programa, la variable s deja de existir.
 
 z = suma(3, 4)
 print("3 + 4 =", z)
